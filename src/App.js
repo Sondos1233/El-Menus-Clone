db4977b1dc1d8e0d22173fa5c6c2fb433a8fba0e
shrouk khaled <shroukkhaled381@gmail.com>
2022-01-28 21:16:06 +0200

    update main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import Footer from './Components/Footer/footer';
+import NavBar from './Components/Navbar/Navbar'
+
 
 function App() {
   return (
     <>
-     
+
+    <NavBar/>
+    <Footer/>
     </>
   );
 }
