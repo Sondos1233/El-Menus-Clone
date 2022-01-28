6b29d8144c27f5754bc49c38c70f5e920846907e
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-01-28 21:02:26 +0200

    merge Footer

---
+++ src/Components/Footer/Cities_list.jsx
@@ -0,0 +1,9 @@
+const Cities = (props) => {
+    return (
+      <>
+         <li><a href="">{props.name}</a></li>
+      </>
+    );
+  };
+  
+  export default Cities;
---
+++ src/Components/Footer/footerF.jsx
@@ -0,0 +1,9 @@
+const About = (props) => {
+    return (
+      <>
+         <li><a href="">{props.name}</a></li>
+      </>
+    );
+  };
+  
+  export default About;
