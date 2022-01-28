bdf2f35291ecf8ecaa8b5a288cfd3357476822dc
Marwa201344 <71200040+Marwa201344@users.noreply.github.com>
2022-01-28 17:24:28 +0200

    Adding LogIn Modal

---
+++ src/Components/logInModal/logInModal.js
@@ -0,0 +1,39 @@
+
+<div class="modal fade" id="LogInModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <div class="title">
+          <img src="images/logo.png" />
+          <p class="mt-1 ms-lg-2">elmenus</p>
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body d-flex">
+        <form id="log" action="#" style="margin-top: 20px;">
+          <div class="user-details">
+              <div class="input-box">
+                  <input type="email" name="email" placeholder="Email" required />
+              </div>
+              <div class="input-box">
+                  <input type="password" name="password" placeholder="Password" required />
+              </div>
+          </div>
+          <div class="button mt-3">
+             <input type="submit" value="Log In" class="btn btn-danger" />
+          </div>
+          <div class="button mt-3">
+            <input type="submit" value="LogIn with Facebook" class="btn " style="background-color: #4267b2; color: white;" />
+         </div>
+         <div class="button mt-3">
+          <input type="submit" value="LogIn with Google" class="btn " style="background-color: #4285f4; color: white;" />
+          </div>
+          <div class="ForgotPass">
+            <p><a href="#" style="font-size: 11px; font-weight: bold; text-decoration: none; color: #e32207;">I forgot my password</a></p>
+          </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+
