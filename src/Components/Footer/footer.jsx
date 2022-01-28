6289244dd418a8f77df5ade3be257c4948022f79
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2022-01-28 12:06:05 +0200

    edit in style

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -8,7 +8,7 @@ import Areas from "./Areas_list";
 import Dishes from "./Dishes_list";
 import About from "./footerF";
 import "./footer_style/footer-style.css";
-
+import '../../Components/main-style.css'
 import Social from "./socialMedia";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
