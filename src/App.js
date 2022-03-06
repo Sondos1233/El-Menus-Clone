a4e644802c0dcec0197ab3f4fd4e066c53353be7
shrouk khaled <shroukkhaled381@gmail.com>
2022-03-06 14:49:09 +0200

    update images in dine out

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import Dineout from './Components/Dineout/Dineout';
+import Navbar from "./Components/Navbar/Navbar";
 import store from "./store/store";
 import Home from './Components/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
-   
+    <Navbar/>
     <Dineout/>
 
     </>
--- src/Components/Dineout/Dineout.jsx
+++ src/Components/Dineout/Dineout.jsx
@@ -206,7 +206,7 @@ export default function Dineout() {
                                                 QueryByMood.map((Type) => {
                                                     return (
                                                      
-                                                        <FilterCard image = "https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/66ada169-d8cd-4021-af20-ad7518ac74d2.jpg" name = {Type.ResName} type= {Type.Mood}></FilterCard>
+                                                        <FilterCard image = {Type.ImageURL} name = {Type.ResName} type= {Type.Mood}></FilterCard>
                                                     )
                                                 })
                                             }
@@ -278,7 +278,7 @@ export default function Dineout() {
                                                 QueryByType.map((Type) => {
                                                     return (
                                                      
-                                                        <FilterCard image = "https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/66ada169-d8cd-4021-af20-ad7518ac74d2.jpg" name = {Type.ResName} type={Type.Type}></FilterCard>
+                                                        <FilterCard image = {Type.ImageURL} name = {Type.ResName} type={Type.Type}></FilterCard>
                                                     )
                                                 })
                                             }
@@ -312,7 +312,7 @@ export default function Dineout() {
 
                                                 Restaurants.map((Res) => {
                                                     return (
-                                                        <DineoutByCity Address={Branches.Adddress} Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/155c7faa-aff2-439f-a4bc-89fd1746258f.jpg"></DineoutByCity>
+                                                        <DineoutByCity Address={Branches.Adddress} Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage={Res.ImageURL} srcLogo = {Res.ImageLogo}></DineoutByCity>
                                                     )
                                                 })
                                             }
@@ -345,7 +345,7 @@ export default function Dineout() {
                                             {
                                                 Restaurants.map((Res) => {
                                                     return (
-                                                        <DineoutByCity Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/de7f35a3-9f76-41ce-8ee1-d0d43bad4e63.jpg"></DineoutByCity>
+                                                        <DineoutByCity Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage={Res.ImageURL} srcLogo={Res.ImageLogo}></DineoutByCity>
                                                     )
                                                 })
                                             }
@@ -380,7 +380,7 @@ export default function Dineout() {
                                                             <div class="box-newResturants" style={{ height: "35vh" }}>
                                                                 <div class="slide-img">
                                                                     <img
-                                                                        src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/66ada169-d8cd-4021-af20-ad7518ac74d2.jpg" style={{ height: "19vh" }}
+                                                                        src={Res.ImageLogo} style={{ height: "19vh" }}
                                                                         alt="" />
                                                                     <div class="detail-box" style={{ flexDirection: "column", justifyContent: "center" }}>
                                                                         <a href="#" class="meal-kind">{Res.ResName}</a>
