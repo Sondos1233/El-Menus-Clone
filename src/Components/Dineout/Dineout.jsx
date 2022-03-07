e09f0419a4a03c65d3f1a231f979354562289635
Amr ahmed <shroukkhaled381@gmail.com>
2022-03-08 01:25:11 +0200

    update model

--- src/Components/Dineout/Dineout.jsx
+++ src/Components/Dineout/Dineout.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Dineout.css'
+import './Dineout.scss'
 import FilterCard from './FilterCard.js/FilterCard';
 import DineoutByCity from './DineoutByCity/DineoutByCity'
 import { useEffect, useState } from 'react';
@@ -221,7 +222,6 @@ export default function Dineout() {
            
 
 
-            {/* Discover BY Types */}
             <section className="disByDishes-Slider container-fluid my-5 overflow-hidden">
                 <div className="row">
                     <div className="col-12 mx-3 py-3">
@@ -253,6 +253,7 @@ export default function Dineout() {
                                                             </div>
                                                         </div>
                                                     </div>
+                                                    
                                                     )
                                                 })
                                             }
@@ -264,26 +265,66 @@ export default function Dineout() {
                         </div>
                     </div>
 
-
                     {/* Filter By Type Display */}
                     <div className="col-12">
-                        <div id="disByDishes-Slider" className="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
+                        <div id="disByDishes-Slider container-fluid" >
+                            <div className="row">
+                                <div className="col-12 d-flex" id="cardDishes">
                                     <div className="row">
-                                        <div className="col-12 d-flex" id="cardDishes">
-                                            {/* <!-- Dieshes Container filled by JS --> */}
+                                        {/* <!-- Dieshes Container filled by JS --> */}
 
-                                            {
-                                                QueryByType.map((Type) => {
-                                                    return (
-                                                     
-                                                        <FilterCard image = {Type.ImageURL} name = {Type.ResName} type={Type.Type}></FilterCard>
-                                                    )
-                                                })
-                                            }
-
-                                        </div>
+                                        {
+                                            QueryByType.map((Type) => {
+                                                {/* <div class="item-1 px-2 p-2">
+                                                        <div class="box-newResturants" style={{ height: "35vh" }}>
+                                                            <div class="slide-img">
+                                                                <img
+                                                                    src={Type.ImageURL} style={{ height: "19vh" }}
+                                                                    alt="" />
+                                                                <div class="detail-box" style={{ flexDirection: "column", justifyContent: "center" }}>
+                                                                    <a href="#" class="meal-kind">{Type.ResName}</a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div> */}
+                                                return (
+                                                    <div className="col-4 my-4">
+                                                        <div className="card">
+                                                            <div className="card-body shadow shadow-lg">
+                                                                <img src={Type.ImageURL}
+                                                                    alt="" className="card-img" style={{ width: "100%", height: "auto"}}/>
+                                                                <div className="Rest-box" style={{display: "flex", flexDirection: "row"}}>
+                                                                    <div className="Rest-logo" style={{ width: "50px", height: "auto", margin: "10px"}}>
+                                                                        <img src={Type.ImageLogo} 
+                                                                        alt="" style={{ width: "100%", height: "auto", border: "2px solid lightgrey", borderRadius: "10px",boxShadow: "1px 1px 5px"}}/>
+                                                                    </div>
+                                                                    <div className= "Rest-data my-2">
+                                                                        <p className="Rest-type m-0" style= {{color: "lightgrey", fontSize: "12px", fontWeight: "700"}}>
+                                                                            European Coffee & Drinks
+                                                                        </p>
+                                                                        <a href= "#" className= "Rest-name m-0" style= {{fontWeight: "700"}}>
+                                                                            {Type.ResName}
+                                                                        </a>
+                                                                        <p className="Rest-rate m-0">
+                                                                            <svg data-v-21f5376e="" data-v-34cbeed1="" height="14" width="14" viewBox="0 0 14 14" class="vue-star-rating-star" step="10"><linearGradient data-v-21f5376e="" id="m6ck1m" x1="0" x2="100%" y1="0" y2="0"><stop data-v-21f5376e="" offset="100%" stop-color="#faad1d"></stop> <stop data-v-21f5376e="" offset="100%" stop-color="#d8d8d8"></stop></linearGradient> <filter data-v-21f5376e="" id="vvdmam" height="130%" width="130%" filterUnits="userSpaceOnUse"><feGaussianBlur data-v-21f5376e="" stdDeviation="0" result="coloredBlur"></feGaussianBlur> <feMerge data-v-21f5376e=""><feMergeNode data-v-21f5376e="" in="coloredBlur"></feMergeNode> <feMergeNode data-v-21f5376e="" in="SourceGraphic"></feMergeNode></feMerge></filter> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#fff" filter="url(#vvdmam)"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#999" stroke-width="0" stroke-linejoin="miter"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)"></polygon></svg>
+                                                                            <svg data-v-21f5376e="" data-v-34cbeed1="" height="14" width="14" viewBox="0 0 14 14" class="vue-star-rating-star" step="10"><linearGradient data-v-21f5376e="" id="m6ck1m" x1="0" x2="100%" y1="0" y2="0"><stop data-v-21f5376e="" offset="100%" stop-color="#faad1d"></stop> <stop data-v-21f5376e="" offset="100%" stop-color="#d8d8d8"></stop></linearGradient> <filter data-v-21f5376e="" id="vvdmam" height="130%" width="130%" filterUnits="userSpaceOnUse"><feGaussianBlur data-v-21f5376e="" stdDeviation="0" result="coloredBlur"></feGaussianBlur> <feMerge data-v-21f5376e=""><feMergeNode data-v-21f5376e="" in="coloredBlur"></feMergeNode> <feMergeNode data-v-21f5376e="" in="SourceGraphic"></feMergeNode></feMerge></filter> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#fff" filter="url(#vvdmam)"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#999" stroke-width="0" stroke-linejoin="miter"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)"></polygon></svg>
+                                                                            <svg data-v-21f5376e="" data-v-34cbeed1="" height="14" width="14" viewBox="0 0 14 14" class="vue-star-rating-star" step="10"><linearGradient data-v-21f5376e="" id="m6ck1m" x1="0" x2="100%" y1="0" y2="0"><stop data-v-21f5376e="" offset="100%" stop-color="#faad1d"></stop> <stop data-v-21f5376e="" offset="100%" stop-color="#d8d8d8"></stop></linearGradient> <filter data-v-21f5376e="" id="vvdmam" height="130%" width="130%" filterUnits="userSpaceOnUse"><feGaussianBlur data-v-21f5376e="" stdDeviation="0" result="coloredBlur"></feGaussianBlur> <feMerge data-v-21f5376e=""><feMergeNode data-v-21f5376e="" in="coloredBlur"></feMergeNode> <feMergeNode data-v-21f5376e="" in="SourceGraphic"></feMergeNode></feMerge></filter> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#fff" filter="url(#vvdmam)"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#999" stroke-width="0" stroke-linejoin="miter"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)"></polygon></svg>
+                                                                            <svg data-v-21f5376e="" data-v-34cbeed1="" height="14" width="14" viewBox="0 0 14 14" class="vue-star-rating-star" step="10"><linearGradient data-v-21f5376e="" id="m6ck1m" x1="0" x2="100%" y1="0" y2="0"><stop data-v-21f5376e="" offset="100%" stop-color="#faad1d"></stop> <stop data-v-21f5376e="" offset="100%" stop-color="#d8d8d8"></stop></linearGradient> <filter data-v-21f5376e="" id="vvdmam" height="130%" width="130%" filterUnits="userSpaceOnUse"><feGaussianBlur data-v-21f5376e="" stdDeviation="0" result="coloredBlur"></feGaussianBlur> <feMerge data-v-21f5376e=""><feMergeNode data-v-21f5376e="" in="coloredBlur"></feMergeNode> <feMergeNode data-v-21f5376e="" in="SourceGraphic"></feMergeNode></feMerge></filter> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#fff" filter="url(#vvdmam)"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)" stroke="#999" stroke-width="0" stroke-linejoin="miter"></polygon> <polygon data-v-21f5376e="" points="6.363636363636363,0.7070707070707071,2.121212121212121,14,12.727272727272727,5.515151515151515,0,5.515151515151515,10.606060606060606,14" fill="url(#m6ck1m)"></polygon></svg>
+                                                                            <span style= {{color: "grey", fontSize: "12px"}}>
+                                                                                1092 Ratings
+                                                                            </span> 
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                                <p className="Rest-address mx-2" style= {{fontWeight: "700", color: "grey"}}>
+                                                                    Royal Maadi Hotel, 11 Road 18, Maadi Sarayat
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                )
+                                            })
+                                        }
                                     </div>
                                 </div>
                             </div>
@@ -296,34 +337,31 @@ export default function Dineout() {
             {/* Discover BY City */}
             <section className="disArear-Slider container-fluid my-5 overflow-hidden">
                 <div className="row">
-
                     <div className="col-12 mx-3 py-3">
                         <h4 className="fw-bold" style={{ color: "rgb(88, 86, 86)" }}>Discover Maadi</h4>
                     </div>
 
-                    <div className="col-12">
-                        <div id="disArear-Slider" className="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
-                            <div className="carousel-inner">
-
-                                <div className="carousel-item active">
-                                    <div className="row">
-                                        <div className="col-12 d-flex">
-                                            {
-
-                                                Restaurants.map((Res) => {
-                                                    return (
-                                                        <DineoutByCity Address={Branches.Adddress} Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage={Res.ImageURL} srcLogo = {Res.ImageLogo}></DineoutByCity>
-                                                    )
-                                                })
-                                            }
-
-                                        </div>
-                                    </div>
+                    <div className="col-12 d-flex">
+                        <div class="Shrouk-Slider slider slider--first js-slider">
+                            <div class="slider__wrapper">
+                                <button class="slider__arrow slider__arrow-prev js-slider-prev" onClick={(e)=>{
+                                        updateSlidePosition(e.nextElementSibling, "prev")
+                                }}>&#8249;</button>
+                                <div class="slider__inner js-slider-inner">
+                                    {
+                                        Restaurants.map((Res) => {
+                                            return (
+                                                <DineoutByCity Address={Branches.Adddress} Rate={Res.Rate} ResType={Res.Type} ResName={Res.ResName} srcImage={Res.ImageURL} srcLogo={Res.ImageLogo}></DineoutByCity>
+                                            )
+                                        })
+                                    }
                                 </div>
+                                <button class="slider__arrow slider__arrow-next js-slider-next" onClick={(e)=>{
+                                        updateSlidePosition(e.previousElementSibling, "next")
+                                }} >&#8250;</button>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </section>
 
--- src/Components/Dineout/DineoutByCity/DineoutByCity.jsx
+++ src/Components/Dineout/DineoutByCity/DineoutByCity.jsx
@@ -8,41 +8,45 @@ export default function DineoutbyCity(props) {
 
     return (
         <>
-         <div class="item-1 " >
-            <div class="box-disArear m-2" style={{ height: "65vh"}}>
-            <div class="slide-img ">
-                <img
-                src={props.srcImage} 
-                alt="" style={{height: "40vh"}}/>
-                <div class="detail-box ">
-                <img
-                    src= {props.srcLogo}
-                    alt="" style={{ width: "50px", maxWidth: "100%", height: "auto", padding: "5px 0"}}/>
-                <div class="px-3">
-                    <p class="resturantDesc m-0" style={{ color: "color: rgb(161, 157, 157)", fontSize: "10px"}}>
-                    {props.ResType}
-                    </p>
-                    <a href="#" class="meal-kind">{props.ResName}</a>
-                    <div class="d-flex">
-                    <p class="resturantRate text-warning">
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                    <i class="fa fa-star" aria-hidden="true"></i>
-                   
-                    <i class="fa fa-star-half" aria-hidden="true"></i>
-                        <span style={{ color: "#474747", fontSize: "10px"}}> {props.Rate} Rating</span>
-                    </p>
-                    
-                    </div>
+            {/* <div class="box-disArear m-2" > */}
+                <div class="slider__slide">
+                    {/* <div class="slide__content"> */}
+                        {/* <div class="slide-img"> */}
+                            <img
+                            src={props.srcImage} 
+                            alt="" style={{height: "300px", width: "100%"}}/>
+                            <div class="detail-box ">
+                            <img
+                                src={props.srcLogo}
+                                alt="" style={{ width: "50px", maxWidth: "100%", height: "auto", padding: "5px 0"}}/>
+                            <div class="px-3">
+                                <p class="resturantDesc m-0" style={{ color: "color: rgb(161, 157, 157)", fontSize: "10px"}}>
+                                {props.ResType}
+                                </p>
+                                <a href="#" class="meal-kind">{props.ResName}</a>
+                                <div class="d-flex">
+                                <p class="resturantRate text-warning">
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                            
+                                <i class="fa fa-star-half" aria-hidden="true"></i>
+                                    <span style={{ color: "#474747", fontSize: "10px"}}> {props.Rate} Rating</span>
+                                </p>
+                                
+                                </div>
+                            </div>
+                                </div>
+                                <div style={{ position: "relative", bottom: "20px", padding: "0 20px"}}>
+                                    <p style= {{ fontSize: "16px", color: "rgb(161, 157, 157)"}}>
+                                        <span style={{ color: "black"}}>Adress</span>: city street
+                                    </p>
+                                </div>
+                        {/* </div> */}
+                    {/* </div> */}
                 </div>
-                    </div>
-                    <div style={{ position: "relative", bottom: "20px", padding: "0 20px"}}>
-                    <p style= {{ fontSize: "16px", color: "rgb(161, 157, 157)"}}>
-                        <span style={{ color: "black"}}>Adress</span>: city street
-                    </p>
-                    </div>
-                </div>
-                </div>
-            </div>
+            {/* </div> */}
+
+            
             {/* <div class="item-1 px-2 ">
                 <div class="box-disArear" >
                     <div class="slide-img">
