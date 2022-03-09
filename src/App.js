9531ad62856c02e2677faf0e2fe01c870e4a94c9
Marwa201344 <71200040+Marwa201344@users.noreply.github.com>
2022-03-09 02:17:17 +0200

    any

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import Userprofile from './Components/userProfile/userProfile'
 import AccountSetting from './Components/accountSetting/accountSetting';
-import{ BrowserRouter as  Router, Route, Routes } from 'react-router-dom';
+import{ BrowserRouter as  Router, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
     <>
 
       <Router>
-        <Routes>
-        <Route exact path="/" element={<Userprofile />} ></Route>
-        <Route path="/accountSetting" element={<AccountSetting />} ></Route>
-        </Routes>
+       
+        <Route exact path="/" element={<Userprofile />} />
+        <Route path="/accountSetting" element={<AccountSetting />} />
+       
       </Router>
 
 
